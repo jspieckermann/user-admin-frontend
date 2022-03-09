015e252818322e81d9392d642b6d34041e4676ac
jspieckermann <jspieckermann@web.de>
2022-03-09 18:29:23 +0100

    Implement standard flow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+      keycloak.init({
+        config: {
+          url: 'http://localhost:8080/',
+          realm: 'user-admin-app',
+          clientId: 'user-admin-frontend',
+        },
+        initOptions: {
+            onLoad: 'login-required'
+        },
+        bearerExcludedUrls: ['/assets']
+      });
+}
 
 @NgModule({
   declarations: [
@@ -14,9 +31,17 @@ import { UserListComponent } from './user-list/user-list.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    KeycloakAngularModule
   ],
-  providers: [],
+  providers: [
+    {
+        provide: APP_INITIALIZER,
+        useFactory: initializeKeycloak,
+        multi: true,
+        deps: [KeycloakService],
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
